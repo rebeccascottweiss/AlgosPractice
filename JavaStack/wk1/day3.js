88c71382fb63667e5b028bf51d44f2fddd1549f9
Rebecca Weiss <rebeccascottweiss@gmail.com>
2020-12-03 08:57:22 -0800

    initial commit

---
+++ JavaStack/wk1/day3.js
@@ -0,0 +1,90 @@
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+
+    // console log (print) the data of every node in the current list
+    read() {
+        var current = this.head; // set curret as the head, if it exists or not
+
+        while (current) { // if current, log and move to current.next
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+
+    // find: return true / false if current list contains a data equal to value
+    contains(value) {
+        // start at the head
+        var runner = this.head;
+
+        // while we have a runner
+        while (runner) {
+
+            // return true if data === value
+            if (runner.data === value) {
+                return true;
+            }
+            // otherwise advance the runner
+            runner = runner.next;
+        }
+
+        // if the while loop completes, return false
+        return false;
+    }
+
+    // Remove from front: remove and return the first node in the SLL
+    removeFromFront() {
+        if (this.isEmpty()) return null; // nothing to remove
+
+        var removed = this.head; // save the head in a temp variable
+        this.head = this.head.next; // move the head
+        removed.next = null; // make removed no longer reference the list
+        return removed;
+    }
+
+    isEmpty() {
+        if (this.head === null) {
+            return true;
+        }
+        return false;
+    }
+
+    addToFront(node) {
+        node.next = this.head;
+        this.head = node;
+    }
+
+    addDataToFront(data) {
+        let node = new Node(data);
+        node.next = this.head;
+        this.head = node;
+    }
+}
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+var myNewSLL = new SLL();
+myNewSLL.addToFront(new Node(5));
+myNewSLL.addToFront(new Node(22));
+myNewSLL.addToFront(new Node(17));
+
+
+var current = myNewSLL.head;
+
+// runner, temp
+
+while (current) {
+    console.log(current.data);
+
+    if (current.next === null) {
+        console.log("this is the last node");
+    }
+
+    current = current.next;
+}
